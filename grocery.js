cc508b736defe742547c50a6ff01f2a9456c2420
Siddartha Punj <sid.punj@gmail.com>
2021-06-19 17:28:57 -0700

    added addfoodintent to skill, not working

--- grocery.js
+++ grocery.js
@@ -15,66 +15,34 @@ const LaunchRequestHandler = {
   },
 
   handle(handlerInput) {
-    const speakOutput = 'Welcome to grocery gather, what items would you like to enter?';
+    const speakOutput = 'Welcome to grocery gather, how can I help?';
     return handlerInput.responseBuilder
       .speak(speakOutput)
       .getResponse();
   }
+};
 
+
+//adding functionality for adding food items to grocery gather's memory
+const AddFoodIntentHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+      && handlerInput.requestEnvelope.intent.name === 'AddFoodIntent';
+  },
+
+  handle(handlerInput) {
+    const checkFoodOutput = 'Adding your items to the cart.';
+    return handlerInput.responseBuilder
+      .speak(checkFoodOutput)
+      .getResponse();
+  }
 };
 
 // The SkillBuilder acts as the entry point for your skill, routing all request and response
 // payloads to the handlers above. Make sure any new handlers or interceptors you've
 // defined are included below. The order matters - they're processed top to bottom.
 exports.handler = Alexa.SkillBuilders.custom()
-  .addRequestHandlers(LaunchRequestHandler)
+  .addRequestHandlers(
+    LaunchRequestHandler,
+    AddFoodIntentHandler)
   .lambda();
-
-
-
-
-/*
-'Product_ValueControlIntent': function() {
-
-},
-
-'AMAZON_NUMBER_ValueControlIntent': function() {
-
-},
-
-'AMAZON_DATE_ValueControlIntent': function() {
-
-},
-
-'CheckFoodIntent': function() {
-
-},
-
-//triggered when user wants to add food to cart
-'AddFoodIntent': function() {
-  this.emit(':tell', 'Hello')
-}*/
-
-
-
-
-
-
-  //'AddProductIntent': function() {
-    //triggered when use wants to add food to cart
-
-    //tell- one way
-    //this.emit(':tell', 'Hello')
-
-    //another way to do same thing
-    //this.response.speak('Hello').cardRenderer('Title', 'Body text')
-    //this.emit(':responseReady')
-
-
-    //asks. Asks again if user doesn't say anything
-    //this.emit(':ask', 'How can I help?', 'You can say something like...')
-
-    //another way to do same thing
-    //this.response.speak('How can i Help?').listen('You can say something like...')
-    //this.emit(':responseReady')
-  //}
