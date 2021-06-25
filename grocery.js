d02a1ba5aa5074fd628367960f7b495b644eef62
Siddartha Punj <sid.punj@gmail.com>
2021-06-25 16:15:50 -0700

    Fixed error for not returning food/count/meal in dialogue

--- grocery.js
+++ grocery.js
@@ -1,54 +1,116 @@
-//
-// Alexa Fact Skill - Sample for Beginners
-//
 
-'use strict'
-// sets up dependencies
 const Alexa = require('ask-sdk-core');
 
 
-
-// core functionality for fact skill
 const LaunchRequestHandler = {
-  canHandle(handlerInput) {
-    return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
-  },
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
+    },
+    handle(handlerInput) {
+        const speakOutput = 'Welcome to grocery gather, how can I help?';
 
-  handle(handlerInput) {
-    const speakOutput = 'Welcome to grocery gather, how can I help?';
-    return handlerInput.responseBuilder
-      .speak(speakOutput)
-      .getResponse();
-  }
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .getResponse();
+    }
 };
 
-
-//adding functionality for adding food items to grocery gather's memory
 const AddFoodIntentHandler = {
-  canHandle(handlerInput) {
-    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-      && handlerInput.requestEnvelope.intent.name === 'AddFoodIntent';
-  },
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && handlerInput.requestEnvelope.request.intent.name === 'AddFoodIntent';
+    },
+    handle(handlerInput) {
+        const food = handlerInput.requestEnvelope.request.intent.slots.food.value;
+        const count = handlerInput.requestEnvelope.request.intent.slots.count.value;
+        const meal = handlerInput.requestEnvelope.request.intent.slots.meal.value;
 
-  handle(handlerInput) {
-    //const checkFoodOutput = 'Adding your items to the cart.';
-    const food = handlerInput.requestEnvelope.request.intent.slots.food.value;
-    const count = handlerInput.requestEnvelope.request.intent.slots.count.value;
-    const meal = handlerInput.requestEnvelope.request.intent.slots.meal.value;
+        const speakOutput = `Thanks, I'll remember that you ate ${count} ${food} at ${meal}`;
 
-    const speakOutput = `Thanks, I'll remember that you ate ${count} ${food} at ${meal}.`;
-
-    return handlerInput.responseBuilder
-      .speak(speakOutput)
-      .getResponse();
-  }
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .getResponse();
+    }
 };
+
+
+
+const SessionEndedRequestHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'SessionEndedRequest';
+    },
+    handle(handlerInput) {
+        // Any cleanup logic goes here.
+        return handlerInput.responseBuilder.getResponse();
+    }
+};
+
+// The intent reflector is used for interaction model testing and debugging.
+// It will simply repeat the intent the user said. You can create custom handlers
+// for your intents by defining them above, then also adding them to the request
+// handler chain below.
+const IntentReflectorHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest';
+    },
+    handle(handlerInput) {
+        const intentName = handlerInput.requestEnvelope.request.intent.name;
+        const speakOutput = `You just triggered ${intentName}`;
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
+            .getResponse();
+    }
+};
+
+// Generic error handling to capture any syntax or routing errors. If you receive an error
+// stating the request handler chain is not found, you have not implemented a handler for
+// the intent being invoked or included it in the skill builder below.
+const ErrorHandler = {
+    canHandle() {
+        return true;
+    },
+    handle(handlerInput, error) {
+        console.log(`~~~~ Error handled: ${error.message}`);
+        const speakOutput = `Sorry, I couldn't understand what you said. Please try again.`;
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    }
+};
+
 
 // The SkillBuilder acts as the entry point for your skill, routing all request and response
 // payloads to the handlers above. Make sure any new handlers or interceptors you've
 // defined are included below. The order matters - they're processed top to bottom.
 exports.handler = Alexa.SkillBuilders.custom()
-  .addRequestHandlers(
-    LaunchRequestHandler,
-    AddFoodIntentHandler)
-  .lambda();
+    .addRequestHandlers(
+        LaunchRequestHandler,
+        AddFoodIntentHandler,
+        //HelpIntentHandler,
+        //CancelAndStopIntentHandler,
+        SessionEndedRequestHandler,
+        IntentReflectorHandler) // make sure IntentReflectorHandler is last so it doesn't override your custom intent handlers
+    .addErrorHandlers(
+        ErrorHandler)
+    .lambda();
+
+
+
+
+
+
+    /*const attributesManager = handlerInput.attributesManager;
+
+    let foodAttributes = {
+        "food": food,
+        "count": defaultCount - count,
+        "meal": meal
+
+    };
+
+    attributesManager.setPersistentAttributes(foodAttributes);
+    await attributesManager.savePersistentAttributes();*/
