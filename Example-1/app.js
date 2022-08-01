c1dab1f9a154eed3957d7a34233be4e0603d9aef
Siddartha Punj <sid.punj@gmail.com>
2022-08-01 16:32:32 -0700

    Added grocery logfile from server, automatically going to safeway.com

--- Example-1/app.js
+++ Example-1/app.js
@@ -1,30 +1,28 @@
-
-//sets up node.js server
-/*const express = require("express");
-const app = express();
-
-app.get("/", function (req, res) {
-    res.sendFile(__dirname + "/index.html");
-});
-
-app.listen(3001, function () {
-    console.log("Server is running on localhost3000");
-});*/
-
 var express = require('express');
 var app = express();
+const fs = require('fs');
 
 app.use(express.json());
 
 app.post('/', function(request, response) {
-  console.log(request.body);
+  const data = request.body;
+  const path = '/home/apunj001/Example-1/logfile.json';
+  dataJson = JSON.parse(JSON.stringify(data));
+  console.log(dataJson);
+
+  fs.writeFile(path, JSON.stringify(dataJson), err => {
+  	if (err) {
+	  console.error(err);
+	}
+  });
+
+  //writing to file
+
+  //console.log(request.body);
   response.send(request.body);
 });
 
-app.listen(8080, '0.0.0.0', function () {
-    console.log("Server is running on localhost 8080");
-});
 
-process.on('uncaughtException', function (err) {
-    console.log(err);
+app.listen(8086, '0.0.0.0', function () {
+    console.log("Server is running on localhost 8080");
 });
---
+++ auto_login.py
@@ -0,0 +1,14 @@
+#first go to safeway webpage
+
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+
+#safeway credentials
+email = "sid.punj@gmail.com"
+password = "Buygrocery#12"
+
+#head to safeway login page
+driver.get("https://www.safeway.com/")
